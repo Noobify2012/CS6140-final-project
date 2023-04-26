e067f6abd523908fccdaad7763f9591a589793ca
bramziltzer <98678934+bramziltzer@users.noreply.github.com>
2023-04-26 14:55:16 -0400

    add saving and loading, add cuda functionality

--- src/final_project/models/feed_forward.py
+++ src/final_project/models/feed_forward.py
@@ -10,7 +10,9 @@ from sklearn.metrics import confusion_matrix
 from torch.utils.data import DataLoader
 from torch.utils.data import TensorDataset
 from pathlib import Path
-import json
+import pickle
+from copy import deepcopy
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class FeedForward(nn.Module):
     def __init__(self, num_hidden_layers:int, num_nodes:int, num_features: int, dropout_prob:float, activation_fn) -> None:
@@ -33,7 +35,8 @@ class FeedForward(nn.Module):
     # TODO add momentum
     def fit(self, train_dataset: TensorDataset, validation_dataset: TensorDataset, batch_size: int, 
             epochs: int, loss_function, learning_rate: float, momentum:float, weight_decay:float):
-        
+        self.to(device)
+        print("using: ","cuda" if torch.cuda.is_available() else "cpu")
         # create dataloader for batching, shuffle to avoid overfitting/batch correlation
         train_dl = DataLoader(train_dataset, batch_size, shuffle=True)
         valid_dl = DataLoader(validation_dataset, batch_size, shuffle=True)
@@ -52,6 +55,7 @@ class FeedForward(nn.Module):
             epoch_validation_loss = 0
 
             for xb, yb in train_dl:
+                xb, yb = xb.to(device), yb.to(device)
                 # run model on batch and get loss
                 # predictions = torch.round(self(xb)).squeeze()
                 # loss = loss_function(predictions, yb.to(torch.float32))
@@ -66,6 +70,7 @@ class FeedForward(nn.Module):
             self.eval()
             with torch.no_grad():
                 for xb_tr, yb_tr in train_dl:
+                    xb_tr, yb_tr = xb_tr.to(device), yb_tr.to(device)
                     # train loss
                     train_predictions = self(xb_tr).squeeze()
                     # train_predictions = torch.round(self(xb_tr)).squeeze()
@@ -73,6 +78,7 @@ class FeedForward(nn.Module):
                     epoch_training_loss += (training_loss * xb_tr.shape[0])
 
                 for xb_val, yb_val in valid_dl:
+                    xb_val, yb_val = xb_val.to(device), yb_val.to(device)
                     # val loss
                     val_predictions = self(xb_val).squeeze()
                     # val_predictions = torch.round(self(xb_val)).squeeze()
@@ -90,14 +96,20 @@ class FeedForward(nn.Module):
 
         return training_losses, validation_losses
 
-    def score(self, tensor_dataset: TensorDataset, batch_size:int=1):
+    def score(self, tensor_dataset: TensorDataset, batch_size:int=64):
+        print("using: ","cuda" if torch.cuda.is_available() else "cpu")
         # reference: https://blog.paperspace.com/training-validation-and-accuracy-in-pytorch/
         self.eval()
+        self.to(device)
         dataloader = DataLoader(tensor_dataset, batch_size, shuffle=True)
         all_predictions = torch.tensor([],dtype=torch.long)
         ground_truth_labels = torch.tensor([],dtype=torch.long)
+        all_predictions, ground_truth_labels = all_predictions.to(device), ground_truth_labels.to(device)
+
         with torch.no_grad():
             for xb, yb in tqdm(dataloader):
+                xb, yb = xb.to(device), yb.to(device)
+
                 # run model on batch
                 class_probabilities = self(xb)
             
@@ -128,6 +140,51 @@ class FeedForward(nn.Module):
             mean_accuracy = correct / len(tensor_dataset)
             
             return mean_accuracy, class_accuracy, classifier_scores, confusion_mtx
+
+    def predict(self, tensor_dataset: TensorDataset, batch_size:int=64):
+        print("using: ","cuda" if torch.cuda.is_available() else "cpu")
+        self.eval()
+        self.to(device)
+        dataloader = DataLoader(tensor_dataset, batch_size, shuffle=True)
+        y_prob = torch.tensor([],dtype=torch.long)
+        ground_truth_labels = torch.tensor([],dtype=torch.long)
+        y_prob, ground_truth_labels = y_prob.to(device), ground_truth_labels.to(device)
+
+        with torch.no_grad():
+            for xb, yb in tqdm(dataloader):
+                xb, yb = xb.to(device), yb.to(device)
+
+                # run model on batch
+                class_probabilities = self(xb)
+            
+                # choose most likely class for each sample
+                probabilities = torch.sigmoid(class_probabilities).squeeze()
+
+                # create running tensor with all true_label, predicted_label pairs 
+                ground_truth_labels = torch.cat((ground_truth_labels, yb.to(torch.float32))).to(torch.int32)
+                y_prob = torch.cat((y_prob, probabilities))
+                y_pred = torch.round(y_prob).to(torch.int32)
+            
+            return ground_truth_labels, y_prob, y_pred
+
+
+
+def plot_frequencies(title, data):
+    
+    fig, ax = plt.subplots(figsize=(6,3))
+    bars = ax.bar(['delayed' if x==1 else 'on time' for x in data[0]], data[1])
+
+    ax.set_title(f'Class Instances in {title.capitalize()} Dataset')
+    ax.set_xlabel('Count')
+    ax.set_ylabel('Labels')
+
+    print(data[1])
+    ax.bar_label(bars, data[1])
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+    # Show the chart
+    plt.show()
 
 def ffn_evaluate(model, train_ds, test_ds, valid_ds):
 # plot losses
@@ -227,19 +284,21 @@ def run_model(param_dict:dict, train_ds, test_ds, valid_ds, num_features:int):
                        "activation_fn":param_dict["activation_fn"]}
     return best_model_params
 
-def save_model_json(model_params:dict):
+
+def save_model_pkl(model_params:dict):
     num_layers = model_params["num_layers"]
     num_nodes = model_params["num_nodes"]
-    filename = Path.cwd().parent / "models" / f"{num_layers}_{num_nodes}_state_dict.json"
-    with open(filename, 'w') as json_file:
-        json.dump(model_params, json_file)
+    filename = Path.cwd().parent / "models" / f"{num_layers}_{num_nodes}_state_dict.pkl"
+    with open(filename, 'wb') as pkl_file:
+        pickle.dump(deepcopy(model_params), pkl_file)
 
 def load_model(filename):
-    with open(filename, 'r') as json_file:
-        params = json.load(json_file)
+    with open(filename, 'rb') as pkl_file:
+        params = pickle.load(pkl_file)
     model = FeedForward(num_hidden_layers=params["num_layers"], 
                     num_nodes=params["num_nodes"], 
                     num_features=params["num_features"], 
                     dropout_prob=params["dropout_prob"], 
                     activation_fn=params["activation_fn"])
     model.load_state_dict(params["state"])
+    return model
